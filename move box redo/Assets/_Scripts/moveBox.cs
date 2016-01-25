6b6454848df38c1aa9f3b0f9f9af3ae562c24e22
bbarlowAIEYr1 <brock.barlow@students.aie.edu.au>
2016-01-25 14:56:33 -0600

    extracted folder
    extracted folder

---
+++ move box redo/Assets/_Scripts/moveBox.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBox : MonoBehaviour {
+
+    public float speed;
+    public bool move, side = false;
+
+    void Start ()
+    {
+        
+	}
+	
+    void Lerp(float a, float b, float c)
+    {
+        gameObject.transform.position = new Vector3(transform.position.x + a, transform.position.y + b, transform.position.z + c);
+    }
+
+	void Update ()
+    {
+        if (Input.GetKey("w"))
+        {
+            move = true;
+        }
+
+        if (move == true && side == false)
+        {
+            Lerp(0, 0, 1 * Time.deltaTime * speed);
+        }
+
+        if (move == true && side == true)
+        {
+            Lerp(0, 0, -1 * Time.deltaTime * speed);
+        }
+
+        if (gameObject.transform.position.z <= -14)
+        {
+            move = false;
+            side = false;
+        }
+
+        if (gameObject.transform.position.z >= -6)
+        {
+            move = false;
+            side = true;
+        }
+    }
+}
---
+++ move box redo/Assets/_Scripts/moveCylinder.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCylinder : MonoBehaviour {
+
+    public float speed;
+    public bool move, side = false;
+
+	void Start ()
+    {
+    
+	}
+
+    void Lerp(float a, float b, float c)
+    {
+        gameObject.transform.position = new Vector3(transform.position.x + a, transform.position.y + b, transform.position.z + c);
+    }
+
+    void Update ()
+    {
+        if (Input.GetKey("a"))
+        {
+            move = true;
+        }
+
+        if (move == true && side == false)
+        {
+            Lerp(-1 * Time.deltaTime * speed, 0, 0);
+        }
+
+        if (move == true && side == true)
+        {
+            Lerp(1 * Time.deltaTime * speed, 0, 0);
+        }
+
+        if (gameObject.transform.position.x >= 3.48f)
+        {
+            move = false;
+            side = false;
+        }
+
+        if (gameObject.transform.position.x <= -3.65f)
+        {
+            move = false;
+            side = true;
+        }
+    }
+}
